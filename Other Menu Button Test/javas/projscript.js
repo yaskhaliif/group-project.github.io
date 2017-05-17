f03422fb1f20e97d9666449c463a6214841c99e7
CMTekCop <davidkhuat7@gmail.com>
2017-05-17 13:16:38 +1000

    Add files via upload

---
+++ Other Menu Button Test/javas/projscript.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	$(".menu").hide();
+	$(".menu-button").click(function(){
+		$(".menu").animate({width: "toggle"
+		});
+		$(".menubt").css("background-color","black")
+	});
+});
+
+//first rime //click = true
+			//if click true = color that
+			//click false
+			//if click false
