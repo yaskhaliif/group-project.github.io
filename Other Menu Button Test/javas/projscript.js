f09701ed98d90e277699fcb0476dc68b231c1eb0
CMTekCop <davidkhuat7@gmail.com>
2017-05-18 14:45:50 +1000

    Add files via upload

--- Other Menu Button Test/javas/projscript.js
+++ Other Menu Button Test/javas/projscript.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
 	$(".menu").hide();
 	$(".actitve").hide();
+	$("#fade").hide();
 	$(".menu-button").click(function(){
 		$(".menu").animate({width: "toggle"});
 		$(".menul").toggleClass("active");
+		$("#fade").fadeToggle();
 	});
 });
